c2a27f2ecb989f6931e8139801fb7c353e9b03bb
Nolen Johnson <johnsonnolen@gmail.com>
2023-04-19 08:27:29 +0700

    sdm660-common: Explicitly disable AVB
    * This allows infra to sign successfully, I hate it, but it builds,
      boots, and works fine enough as a workaround.
    
    * The underlying issue is that this flag causes avbtool args and location
      to be set in `misc_info.txt` whereas before it was not.
    
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -442,9 +442,8 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
 # Verity
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/system
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/vendor
-$(call inherit-product, build/target/product/verity.mk)
+# Only needed for signing
+BOARD_AVB_ENABLE := false
 
 # Vibrator
 PRODUCT_PACKAGES += \
